15aba99603ee2542f47760c0a21aa8bb3e0e3cbb
Nicolas <nicolas__du95@hotmail.fr>
2024-02-11 20:20:48 +0100

    UndirectedGraph

---
+++ graph_utils.py
@@ -0,0 +1,43 @@
+from typing import Union, Tuple
+from graph import Graph, Vertex
+from collections import deque, defaultdict
+
+def invert_dict(dico: dict) -> dict:
+    res = defaultdict(list)
+    
+    for k, v in dico.items():
+        res[v].append(k)
+    
+    return dict(res)
+
+def is_bipartite(graph: Graph) -> Union[Tuple[Tuple[Vertex], Tuple[Vertex]], bool]:
+    """Check if a graph is bipartite and return the two partitions if it is
+
+    Args:
+        graph (Graph): Graph to check
+
+    Returns:
+        Union[Tuple[Tuple[Vertex], Tuple[Vertex]], bool]: 
+            - If the graph is bipartite, the two partitions
+            - If the graph is not bipartite, False
+    """
+
+    queue = deque()
+    bipartition = defaultdict(lambda : -1)
+    start = graph.get_vertices()[0]
+    
+    queue.append(start)
+    bipartition[start] = False
+
+    while queue:
+        u = queue.popleft()
+        for v in graph.get_neigbours(u):
+            if bipartition[v] == -1: # If not visited
+                queue.append(v)
+                bipartition[v] = not bipartition[u]
+            elif bipartition[v] == bipartition[u]:
+                return False
+
+    d = invert_dict(bipartition)
+
+    return tuple(d[False]), tuple(d[True])
