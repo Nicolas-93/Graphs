d7c3265510f12589aaa6a918a7b360a79b53ede0
Nicolas <nicolas__du95@hotmail.fr>
2024-02-22 22:31:54 +0100

    Animation breakpoints

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -2,3 +2,4 @@ from graphs.impl import Edge, WeightedEdge, Vertex, Graph
 from graphs.algorithms import (
     is_bipartite
 )
+from graphs.utils import set_anim_breakpoint_hook
--- graphs/utils.py
+++ graphs/utils.py
@@ -1,4 +1,15 @@
 from collections import defaultdict
+from typing import Callable
+
+ANIM_BREAKPOINT_HOOK = None
+
+def set_anim_breakpoint_hook(hook: Callable):
+    global ANIM_BREAKPOINT_HOOK
+    ANIM_BREAKPOINT_HOOK = hook
+
+def abreakpoint(state, *args, **kwargs):
+    if ANIM_BREAKPOINT_HOOK is not None:
+        ANIM_BREAKPOINT_HOOK(state, args, kwargs)
 
 def invert_dict(dico: dict) -> dict:
     res = defaultdict(list)
