ab04c7a790f88772c59430fe2c0b9a530d5f3ec5
Nicolas <nicolas__du95@hotmail.fr>
2024-05-01 17:38:41 +0200

    Prim's algorithm with natural weight

--- graphs/algorithms/__init__.py
+++ graphs/algorithms/__init__.py
@@ -1,6 +1,6 @@
 from ..algorithms.bipartitions import is_bipartite
 from ..algorithms.mwst_prim import mwst_prim, PrimAnimStep
-from ..algorithms.mwst_kruskal import mwst_kruskal
+from ..algorithms.mwst_kruskal import mwst_kruskal, mwst_kruskal_natural_weights
 from ..algorithms.traversal import (
     breadth_first, breadth_first_tree,
     depth_first, depth_first_tree
--- graphs/algorithms/mwst_kruskal.py
+++ graphs/algorithms/mwst_kruskal.py
@@ -2,6 +2,8 @@ from ..basic import UndirectedGraph
 from ..weighted import WeightedUndirectedGraph
 from unionfind import unionfind
 from graphs import UDEdge
+from itertools import chain
+
 
 def mwst_kruskal(graph: WeightedUndirectedGraph) -> WeightedUndirectedGraph:
     forest = WeightedUndirectedGraph()
@@ -19,3 +21,29 @@ def mwst_kruskal(graph: WeightedUndirectedGraph) -> WeightedUndirectedGraph:
             classes.unite(c_u, c_v)
 
     return forest
+
+def mwst_kruskal_natural_weights(graph: WeightedUndirectedGraph) -> WeightedUndirectedGraph:
+    forest = WeightedUndirectedGraph()
+    forest.add_vertices(graph.get_vertices())
+    classes = unionfind(graph.get_nb_vertex())
+
+    vertices = {vertex : n for n, vertex in enumerate(graph.get_vertices())}
+    max_weight = max(graph.get_weighted_edges()).weight
+    edges = [None for _ in range(max_weight + 1)]
+
+    for edge in graph.get_weighted_edges():
+        if edges[edge.weight] is None:
+            edges[edge.weight] = []
+        edges[edge.weight].append(edge)
+    
+    print(vertices)
+
+    for edge in chain(*filter(lambda x: x is not None, edges)):
+        c_u, c_v = classes.find(vertices[edge.u]), classes.find(vertices[edge.v])
+        if c_u != c_v:
+            forest.add_edge(edge)
+            classes.unite(c_u, c_v)
+
+    return forest
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
